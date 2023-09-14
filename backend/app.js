23ef7ada12d66846a51419632e0481b0e0705ea5
Droubi1 <105873915+Droubi1@users.noreply.github.com>
2023-09-14 17:13:52 +0200

    feature: connected app to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const path = require("path");
+
 const app = express();
+
+require("dotenv").config();
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Connexion à MongoDB réussi !"))
+    .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 module.exports = app;
