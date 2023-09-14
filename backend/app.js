bd7baa12437af00cc69e1d465c91fe2b7fdbf3f1
Droubi1 <105873915+Droubi1@users.noreply.github.com>
2023-09-14 18:35:11 +0200

    feature: middleware for cors and models for user and books added

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,28 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const path = require("path");
+const handleCors = require("./middlewares/handleCors");
 
 const app = express();
 
 require("dotenv").config();
 
+// Connexion Database
 mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => console.log("Connexion à MongoDB réussi !"))
     .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+// Middleware pour gérer les erreurs CORS
+app.use(handleCors);
+
+// Routes
+// app.use("/api/books", bookRoutes);
+// app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
---
+++ backend/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+});
+
+// Facilite la gestion des erreurs d'unicité de l'email
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
