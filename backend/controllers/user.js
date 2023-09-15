31a7c38498e8db8bb101d5817f0980a5eb894394
Droubi1 <105873915+Droubi1@users.noreply.github.com>
2023-09-15 17:14:51 +0200

    feature: login function controller added

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,6 +3,8 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
+require("dotenv").config();
+
 // Gère la création de compte utilisateur
 exports.signup = (req, res, next) => {
     // Vérification des règles courantes du mot de passe
@@ -34,4 +36,37 @@ exports.signup = (req, res, next) => {
 };
 
 // Gère la connexion
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        // Cherche l'email dans la base de données et le stock dans la promesse "user"
+        .then((user) => {
+            if (user === null) {
+                res.status(401).json({
+                    message: "Paire identifiant / mot de passe incorrect",
+                });
+            } else {
+                bcrypt
+                    .compare(req.body.password, user.password)
+                    .then((valid) => {
+                        if (!valid) {
+                            res.status(401).json({
+                                message:
+                                    "Paire identifiant / mot de passe incorrect",
+                            });
+                        } else {
+                            res.status(200).json({
+                                userId: user._id,
+                                // Crée un JSON Web Token pour l'authentification et l'autorisation
+                                token: jwt.sign(
+                                    { userId: user._id },
+                                    process.env.JWT_SECRET_KEY,
+                                    { expiresIn: "4h" }
+                                ),
+                            });
+                        }
+                    })
+                    .catch((error) => res.status(500).json({ error }));
+            }
+        })
+        .catch((error) => res.status(500).json({ error }));
+};
