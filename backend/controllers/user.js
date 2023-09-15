62f37f8888d5004e80159a1148de170a2249cf04
Droubi1 <105873915+Droubi1@users.noreply.github.com>
2023-09-15 12:37:13 +0200

    feature: signup controller edited with password hash and rules

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,9 +3,35 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
-/**
- * POST
- */
-exports.signup = (req, res, next) => {};
+// Gère la création de compte utilisateur
+exports.signup = (req, res, next) => {
+    // Vérification des règles courantes du mot de passe
+    const passwordRules =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#]).{8,}$/;
 
+    if (!passwordRules.test(req.body.password)) {
+        return res.status(400).json({
+            message:
+                "Le mot de passe doit contenir au moins 8 caractères, dont une majuscule, une minuscule, un chiffre et un caractère spécial.",
+        });
+    }
+
+    // Hashage du mot de passe
+    bcrypt
+        .hash(req.body.password, 10)
+        .then((hash) => {
+            const user = new User({
+                email: req.body.email,
+                password: hash,
+            });
+            user.save()
+                .then(() =>
+                    res.status(201).json({ message: "Utilisateur créé !" })
+                )
+                .catch((error) => res.status(400).json({ error }));
+        })
+        .catch((error) => res.status(500).json({ error }));
+};
+
+// Gère la connexion
 exports.login = (req, res, next) => {};
