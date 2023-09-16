3a72086ee7b7e3a840e00c771c3a183a2004d7b3
Droubi1 <105873915+Droubi1@users.noreply.github.com>
2023-09-16 10:25:19 +0200

    fix: added no space allowed for passwords in regex middleware

--- backend/middlewares/password-validator.js
+++ backend/middlewares/password-validator.js
@@ -1,12 +1,12 @@
 // Vérification des règles courantes du mot de passe
 module.exports = (req, res, next) => {
     const passwordRules =
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#]).{8,}$/;
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])(?!.*\s).{8,}$/;
 
     if (!passwordRules.test(req.body.password)) {
         return res.status(400).json({
             message:
-                "Le mot de passe doit contenir au moins 8 caractères, dont une majuscule, une minuscule, un chiffre et un caractère spécial.",
+                "Le mot de passe doit contenir au moins 8 caractères, dont une majuscule, une minuscule, un chiffre, un caractère spécial et ne doit contenir aucun espace.",
         });
     }
     next();
