ce5c87cb7e3c7d6895558b27fd262313fbba9619
Droubi1 <105873915+Droubi1@users.noreply.github.com>
2023-09-16 20:00:05 +0200

    feature: added sharp dependencie to reduce cover pictures weight

---
+++ backend/middlewares/sharp-config.js
@@ -0,0 +1,25 @@
+const sharp = require("sharp");
+const fs = require("fs");
+
+module.exports = async (req, res, next) => {
+    if (!req.file) return next();
+
+    try {
+        await sharp(req.file.path)
+            .resize(206, 260, { fit: sharp.fit.inside })
+            .webp({ quality: 80 })
+            .toFile(`${req.file.path.split(".")[0]}.webp`);
+
+        // Supprimer l'image originale
+        fs.unlink(req.file.path, (error) => {
+            req.file.path = `${req.file.path.split(".")[0]}.webp`;
+            if (error) {
+                console.log(error);
+            }
+        });
+
+        next();
+    } catch (error) {
+        res.status(500).json({ error: "Impossible d'optimiser l'image" });
+    }
+};
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const auth = require("../middlewares/auth");
 const multer = require("../middlewares/multer-config");
+const sharp = require("../middlewares/sharp-config");
 
 const bookCtrl = require("../controllers/book");
 
@@ -10,7 +11,7 @@ router.get("/", bookCtrl.getAllBooks);
 router.get("/:id", bookCtrl.getOneBook);
 router.get("/bestrating", bookCtrl.getBestBook);
 
-router.post("/", auth, multer, bookCtrl.createBook);
+router.post("/", auth, multer, sharp, bookCtrl.createBook);
 router.post("/:id/rating", auth, bookCtrl.rateBook);
 
 router.put("/:id", auth, multer, bookCtrl.updateBook);
