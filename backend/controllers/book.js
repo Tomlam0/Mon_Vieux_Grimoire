d58bba62ed7f1d385544f89f8b4280d229f62f05
Droubi1 <105873915+Droubi1@users.noreply.github.com>
2023-09-19 15:39:07 +0200

    feature: added updateBook logic controller

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -69,7 +69,33 @@ exports.getAllBooks = (req, res, next) => {
 /**
  * PUT
  */
-exports.updateBook = (req, res, next) => {};
+exports.updateBook = (req, res, next) => {
+    const bookObject = req.file
+        ? // Si modification de l'image on copie les nouvelles données
+          {
+              ...JSON.parse(req.body.book),
+              imageUrl: `${req.protocol}://${req.get("host")}/images/${
+                  req.file.filename.split(".")[0]
+              }.webp`,
+          }
+        : { ...req.body };
+
+    delete bookObject._userId;
+
+    Book.findOne({ _id: req.params.id })
+        .then((book) => {
+            if (book.userId != req.auth.userId) {
+                res.status(403).json({ message: "Utilisateur non autorisé" });
+            } else {
+                Book.updateOne({ _id: req.params.id }, { ...bookObject })
+                    .then(() =>
+                        res.status(200).json({ message: "Livre modifié" })
+                    )
+                    .catch((error) => res.status(400).json({ error }));
+            }
+        })
+        .catch((error) => res.status(500).json({ error }));
+};
 
 /**
  * DELETE
